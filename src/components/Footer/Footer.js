d8c0348472d96f1814890010bbab3c4a2df00bc4
darp-cs <diegoarod14@gmail.com>
2021-02-08 20:38:39 -0500

    Tweaks

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,9 +7,9 @@ const footer = () =>(
 
     <div className = {classes.FooterContainer}>
         <ul className = {classes.List}>
-            <li className = {classes.Insta}>Instagram</li>
-            <li className = {classes.Twitter}>Twitter</li>
-            <li className = {classes.Copyright}>Copyright</li>
+            <li className = {classes.Insta}><a  target="_blank" href = "https://www.twitter.com">Instagram</a></li>
+            <li className = {classes.Twitter}><a href = "https://www.instagram.com"  target="_blank">Twitter</a></li>
+            <li className = {classes.Copyright}>Copyright &copy;</li>
         </ul>
     </div>
 )
